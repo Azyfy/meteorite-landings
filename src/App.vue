6a65b03f153ce82f673bce9c4ead48e5da6110b1
Azryel <AzryelShroud@gmail.com>
2021-09-30 15:59:53 +0200

    add initial object, light and helpers with three.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,83 @@
 <template>
+  <canvas ref="canvasEl" class="bg"></canvas>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+
   </div>
   <router-view/>
 </template>
+
+<script>
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+import { ref, onMounted } from "vue"
+
+export default {
+  setup() {
+    const canvasEl = ref(null)
+
+    onMounted(() => {
+        console.log("here", canvasEl.value) 
+
+         const scene = new THREE.Scene()  // scene == container
+
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 ) // args; field of view, aspect ratio, view frustrum
+
+        const renderer = new THREE.WebGLRenderer({
+          canvas: canvasEl.value,
+        }) //render
+
+        renderer.setPixelRatio( window.devicePixelRatio )
+        renderer.setSize( window.innerWidth, window.innerHeight )
+        camera.position.setZ(30)
+
+        renderer.render( scene, camera ) // draw
+
+        const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
+        const material = new THREE.MeshStandardMaterial( { color: 0xDD6347 }) //wrapping paper for object; meshbasicmaterial with wireframe prop true
+        const torus = new THREE.Mesh( geometry, material )
+
+        scene.add(torus)
+
+        const pointLight = new THREE.PointLight(0xffffff) //light in all directions
+        pointLight.position.set(5, 5, 5)
+
+        const ambientLight = new THREE.AmbientLight(0xffffff) //light everything equaly
+
+        scene.add(pointLight, ambientLight)
+
+        const lightHelper = new THREE.PointLightHelper(pointLight)  //show position of pointlight
+        const gridHelper = new THREE.GridHelper(200, 50)
+
+        scene.add(lightHelper, gridHelper)
+
+        const controls = new OrbitControls(camera, renderer.domElement) //update camera position while mouse click dragging 
+
+        const animate = () => {
+          requestAnimationFrame(animate)
+
+          torus.rotation.x += 0.01
+          torus.rotation.y += 0.005
+          torus.rotation.z += 0.01
+
+          controls.update()
+
+          renderer.render( scene, camera )
+        }
+
+        animate()
+
+      })
+    
+
+
+    
+
+    return { canvasEl }
+  },
+}
+</script>
 
 <style>
 #app {
@@ -15,16 +88,10 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
