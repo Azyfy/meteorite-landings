a3ab7eac2a27a85dd630d88dd8d2c5e5cc147cdf
Azryel <AzryelShroud@gmail.com>
2021-10-01 19:12:13 +0200

    make details draggable

--- src/components/MeteorDetails.vue
+++ src/components/MeteorDetails.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="meteor-details">
-      <h2> {{ meteor.name }} </h2>
+  <div id="meteor-details">
+      <div id="meteor-details-h2" >
+           <h2 > {{ meteor.name }} </h2>
+      </div>
       <p> Mass: {{ meteor.mass }} g </p>
       <p> Year: {{ meteor.year.slice(0, 10) }}  </p>
       <p> Status: {{ meteor.fall }}  </p>
@@ -11,17 +13,63 @@
 </template>
 
 <script>
+
 export default {
     props: ["meteor"],
+    mounted() {
+
+      dragElement(document.getElementById("meteor-details"))
+
+      function dragElement(el) {
+
+        let p1 = 0, p2 = 0, p3 = 0, p4 = 0
+
+        if (document.getElementById(el.id)) {
+          document.getElementById(el.id).onmousedown = dragMouseDown
+        } 
+
+        function dragMouseDown(e) {
+
+          p3 = e.clientX
+          p4 = e.clientY
+
+          document.onmouseup = stopDragElement
+          document.onmousemove = elementDrag
+        }
+
+        function elementDrag(e) {
+
+          p1 = p3 - e.clientX
+          p2 = p4 - e.clientY
+          p3 = e.clientX
+          p4 = e.clientY
+
+          el.style.top = (el.offsetTop - p2) + "px"
+          el.style.left = (el.offsetLeft - p1) + "px"
+        }
+
+        function stopDragElement() {
+          document.onmouseup = null;
+          document.onmousemove = null;
+        }
+      }
+
+    }
 }
 </script>
 
 <style>
-    .meteor-details {
+    #meteor-details {
         position: absolute;
         left: 50%;
         margin: auto;
         z-index: 2;
         color: red;
     }
+
+    #meteor-details-h2 {
+        border: 1px solid purple;
+    }
+
+
 </style>
